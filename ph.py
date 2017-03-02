ecd57af700bc886c9cef0a95558469e0041049cb
Alec Savoy <savoy.alec@yahoo.com>
2017-03-02 12:57:27 -0800

    Updated ph
    Changed ph to be functional

--- ph.py
+++ ph.py
@@ -1,35 +1,17 @@
 #!/CConjecture/ph.py
-from datetime import date
 
-
-class Tree:
-    def __init__(self, rounds):
-        self.date = date.today()
-        self.id = tuple(self.date)
-        self.rounds = rounds
-        self.row = []
-        self.rows = 0
-        self.grid = {}
-
-    def getid(self):
-        return self.id
-
-    def getRounds(self):
-        return self.rounds
-
-    def getRows(self):
-        return self.rows
-
-
-class Node:
-    def __init__(self, num, row, parent1, parent2=False):
+class Num:
+    def __init__(self, num, isElbow, parent1, parent2=False):
         self.num = num
         self.parent1 = parent1
-        self.row = row
+        self.isElbow = isElbow
         if parent2:
             self.parent2 = parent2
         else:
-            self.parent2 = False
+            self.parent2 = None
+
+    def __str__(self):
+        return str(num)
 
     def getNum(self):
         return self.num
@@ -38,22 +20,49 @@ class Node:
         return self.parent1
 
     def getParentTwo(self):
-        if self.parent2:
-            return self.parent2
-        else:
-            return "0"
+        return self.parent2
 
-    def getRow(self):
-        return self.row
+    def getIsElbow(self):
+        return self.isElbow
 
 
-def main(rounds):
-    while rounds != 0:
-        counter = 0
-        n = 1
-        rowName = "row" + str(n)
+def genParentRow(n):
+    row = {}
+    parent1 = 0
+    counter = 1
+    for k in range(n):
+        name = counter
+        counter += 1
+        
+        num = 4**k
+        num1 = Num(num,True,parent1)
+        row[name] = num1
+
+        name = counter
+        counter += 1
+
+        num = 2*num
+        parent1 = num1.getNum()
+        num2 = Num(num,False,parent1)
+        row[name] = num2
+
+        parent1 = num2.getNum()
+
+    for entry in row:
+        print()
+        print(entry)
+        print(row[entry].getNum())
+    return None
 
 
+def genMatrix():
+    return None
+
+
+
+
+
+def main():
     return None
 
 
@@ -70,9 +79,10 @@ def higherGen(num):
 
 
 def run():
-    print(date.today())
+    #print(date.today())
     print("there will be a parental simulation here")
     main()
     return None
 
 run()
+genParentRow(5)
