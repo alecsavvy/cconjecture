0582474e9a8e232301c0294da48d0b12672a6e31
Alec Savoy <asavoy@cs.uoregon.edu>
2017-03-06 13:43:39 -0800

    added fives test file

---
+++ fives.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+n = 5
+for num in range(30):
+    n2 = n -1
+    if n2 % 3 == 0:
+        print(str(num))
+        print("works: " + str(n))
+        print("parent: " + str(n2))
+        n *= 2
+    else:
+        print(str(num))
+        print("doesn't work: " + str(n))
+        n *= 2
